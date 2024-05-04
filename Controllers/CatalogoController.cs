dc4e022354a152e2409242fc2d2b35c212f67bd0
lynnerv <aythor_briceno@usmp.pe>
2024-05-04 03:35:42 -0500

    ver detalles y buscador

--- Controllers/CatalogoController.cs
+++ Controllers/CatalogoController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using grupo_rojo.Data;
 using Microsoft.EntityFrameworkCore;
+using grupo_rojo.Models;
 
 namespace grupo_rojo.Controllers
 {
@@ -21,7 +22,7 @@ namespace grupo_rojo.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string? searchString)
         {
             // Obtener todos los productos de la base de datos
             var productos = _context.DataProducto.ToList();
@@ -31,7 +32,22 @@ namespace grupo_rojo.Controllers
             {
                 return NotFound(); // Puedes manejar este caso según tus necesidades
             }
-            return View(productos);
+             // Aplicar filtro de búsqueda si el parámetro searchString no está vacío
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                productos = productos
+                    .Where(s => s.Articulo.IndexOf(searchString, StringComparison.OrdinalIgnoreCase) >= 0)
+                    .ToList();
+            }
+            return View(productos); // Pasar la lista de productos a la vista
+        }
+
+        public async Task<IActionResult> Details(int? id){
+            Producto objProduct = await _context.DataProducto.FindAsync(id);
+            if(objProduct == null){
+                return NotFound();
+            }
+            return View(objProduct);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
